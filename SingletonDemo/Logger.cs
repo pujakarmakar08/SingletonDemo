9835eed7ae6f1f8ec4a9b15c055c7c70dbc1c39e
pujak <pkarmakar@INBAWN166806>
2023-03-30 15:46:06 +0530

    Add project files.

---
+++ SingletonDemo/Logger.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SingletonDemo {
+    public sealed class Logger {
+        private static int counter = 0;
+        public static readonly Lazy<Logger> logger = new Lazy<Logger>(() => new Logger());
+
+        public static Logger GetLogger => logger.Value;
+       
+
+        private Logger() {
+            counter++;
+            Console.WriteLine("Counter value is: " +  counter.ToString());
+
+        }
+        public void PrintDetails(string message) {
+            Console.WriteLine(message);
+        }
+    }
+}
---
+++ SingletonDemo/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SingletonDemo {
+    class Program {
+        static void Main(string[] args) {
+            Parallel.Invoke(() => PrintLog1(),
+                () => PrintLog2());
+            Console.ReadLine();
+        }
+
+        private static void PrintLog2() {
+            var log2 = Logger.GetLogger;
+            log2.PrintDetails("Log 2 message");
+        }
+
+        private static void PrintLog1() {
+            var log1 = Logger.GetLogger;
+            log1.PrintDetails("Log 1 message");
+        }
+    }
+}
